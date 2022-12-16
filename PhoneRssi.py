3cb4a5a47c07e4c23aecb178c570cc1a0cb6c718
howoldilya <ilyashechka233@gmail.com>
2022-12-16 17:33:27 +0300

    get rssi updated method v 0.1

--- PhoneRssi.py
+++ PhoneRssi.py
@@ -6,12 +6,17 @@ from bleak import BleakScanner
 from bleak.backends.device import BLEDevice
 from bleak.backends.scanner import AdvertisementData
 
+
+# для работы требуются библиотеки pybluez, asyncio, bleak
+# определяет девайсы с low energy bluetooth
+# для определения телефона нужно запустить nRF Connect на телефоне
+# и создать новый сервис который будет раздавать сигнал по нужной технологии
 logger = logging.getLogger(__name__)
 
-
+#логгер выводит доступную информацию о девайсе
 def simple_callback(device: BLEDevice, advertisement_data: AdvertisementData):
     logger.info(f"{device.address}: {advertisement_data}")
-
+#основной метод возвращает мощность сигнала девайса/девайсов по порядку в дбМ
 def getRssi(device: BLEDevice, advertisement_data : AdvertisementData):
     print(device.name)
     print(device.rssi)
@@ -19,7 +24,7 @@ def getRssi(device: BLEDevice, advertisement_data : AdvertisementData):
     return device.rssi
 
 
-
+#каждые 5 секунд происходит поиск девайсов
 async def main(service_uuids):
     scanner = BleakScanner( getRssi, service_uuids)
 
@@ -28,8 +33,7 @@ async def main(service_uuids):
         await scanner.start()
         await asyncio.sleep(5.0)
         await scanner.stop()
-
-
+# ассинхронно запускает сканнер и записывает айдишники девайсов
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
